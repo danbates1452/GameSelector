e33a00b07b10c71f4a0e1004d684a7db0b1963bf
Daniel Bates <42376377+danbates1452@users.noreply.github.com>
2023-03-26 20:00:10 +0100

    multiple refinements
    removed iteration
    fixed a bug where names including full stops were cropped too soon
    now removes repeats then loops back through games

--- GameSelector.py
+++ GameSelector.py
@@ -1,33 +1,47 @@
 import os
 import copy
-import random
+from random import randint, shuffle
 
 path = "D:\\GameShortcuts" #shortcut directory
+# Constants
+files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+shuffle(files)
+shortcuts = copy.deepcopy(files)
 
-def getRandGame():
-    onlyfiles = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
-    shortcuts = copy.deepcopy(onlyfiles)
-    for f in range(0, len(onlyfiles)):
-        onlyfiles[f] = onlyfiles[f].split('.')[0]
-    rand = random.randint(0, len(onlyfiles))
+
+# Variables
+unseen = copy.deepcopy(shortcuts)
+attempt_count = 0
+
+def remove_file_extension(filename):
+    return filename.rsplit('.', 1)[0]
+
+def get_random_game():
+    global shortcuts, unseen, attempt_count
+    if (len(unseen)) == 0:
+        unseen = copy.deepcopy(shortcuts)
+        attempt_count = 0
+
+    rand = randint(0, len(unseen) - 1)
+
+    attempt_count += 1
+    how_many_seen = ''.join(['(', str(attempt_count),'/', str(len(shortcuts)), ')'])
     print('\n')
-    print(onlyfiles[rand])
-    return shortcuts[rand]
+    print(remove_file_extension(unseen[rand]), how_many_seen)
 
+    return unseen.pop(rand)
 
 while True:
-    randGameShortcut = getRandGame()
+    randGameShortcut = get_random_game()
     print('Press [Y] and [Enter] to play')
     print('Press [N] and [Enter] to re-roll')
-    print('\n\nPress [Enter] to exit this window')
+    print('\n\nPress just [Enter] to exit this window')
     response = input('\n')
     if ('y' in response) or ('Y' in response):
         os.startfile(path + "\\" + randGameShortcut)
-        break
+        break #break the loop (end the script here)
     elif ('n' in response) or ('N' in response):
-        #print("\r")
-        #print("--------------------------------\n")
-        os.system('cls') #windows only
-        continue
+        os.system('cls') #windows only - clear the console
+        continue #continue looping
     else:
-        break
+        break #break the loop (end the script here)
